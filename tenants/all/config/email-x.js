407619a4e543578b9e4f49116026ae6ffeb021fe
Jessica Wade <jessica.wade@parameter1.com>
2021-10-26 14:36:40 -0500

    Add tenants/config folder

---
+++ tenants/all/config/email-x.js
@@ -0,0 +1,7 @@
+// const EmailXConfiguration = require('@parameter1/base-cms-marko-newsletters-email-x/config');
+
+// const config = new EmailXConfiguration(process.env.EMAILX_SERVE_URI || 'https://roguemonkeymedia.serve.email-x.parameter1.com');
+
+// config
+
+// module.exports = config;
---
+++ tenants/all/config/native-x.js
@@ -0,0 +1,5 @@
+// module.exports = {
+//   uri: 'https://roguemonkeymedia.native-x.parameter1.com',
+//   placements: {
+//   }
+// };
