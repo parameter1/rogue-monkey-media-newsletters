b8e7cf21c5a26f8df11f1e8e15c0635f9da68349
Jessica Wade <jessica.wade@parameter1.com>
2021-11-02 09:49:35 -0500

    Create ad slots for NRWA newsletter

--- tenants/all/config/email-x.js
+++ tenants/all/config/email-x.js
@@ -1,7 +1,27 @@
-// const EmailXConfiguration = require('@parameter1/base-cms-marko-newsletters-email-x/config');
+const EmailXConfiguration = require('@parameter1/base-cms-marko-newsletters-email-x/config');
 
-// const config = new EmailXConfiguration(process.env.EMAILX_SERVE_URI || 'https://roguemonkeymedia.serve.email-x.parameter1.com');
+const config = new EmailXConfiguration(process.env.EMAILX_SERVE_URI || 'https://roguemonkeymedia.serve.email-x.parameter1.com');
 
-// config
+config
+  .setAdUnits('rural-water-wire', [
+    {
+      name: 'ad-slot-1',
+      id: '61795c6f1cae6a6ce858a42b',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'ad-slot-2',
+      id: '61795cfb1cae6adea058a435',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'ad-slot-3',
+      id: '61795d0f1cae6a231e58a451',
+      width: 600,
+      height: 100,
+    },
+  ]);
 
-// module.exports = config;
+module.exports = config;
--- tenants/all/config/native-x.js
+++ tenants/all/config/native-x.js
@@ -1,5 +1,9 @@
-// module.exports = {
-//   uri: 'https://roguemonkeymedia.native-x.parameter1.com',
-//   placements: {
-//   }
-// };
+module.exports = {
+  uri: 'https://roguemonkeymedia.native-x.parameter1.com',
+  placements: {
+    'rural-water-wire': {
+      'native-slot-1': '61795e75338044000184c29f',
+      'native-slot-2': '61795eec338044000184c2b2',
+    },
+  },
+};
