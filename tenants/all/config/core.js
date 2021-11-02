6ea73ee49bebf99eee93fbb0900c2982f51667a7
Jessica Wade <jessica.wade@parameter1.com>
2021-11-02 09:50:01 -0500

    Add rural-water-wire NL to core.js file

--- tenants/all/config/core.js
+++ tenants/all/config/core.js
@@ -1,15 +1,19 @@
-// const brands = require('./brands');
-// const emailX = require('./email-x');
-// const nativeX = require('./native-x');
+const brands = require('./brands');
+const emailX = require('./email-x');
+const nativeX = require('./native-x');
 
-// const config = {
-//   emailX,
-//   nativeX,
-//   dpm: {
-//     emailx: {
-//       enabled: true,
-//     },
-//   },
-// };
+const config = {
+  emailX,
+  nativeX,
+  dpm: {
+    emailx: {
+      enabled: true,
+    },
+  },
+  'rural-water-wire': {
+    name: 'Newsletter',
+    ...brands.nrwa,
+  },
+};
 
-// module.exports = config;
+module.exports = config;
