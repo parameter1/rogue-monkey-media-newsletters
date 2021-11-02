b2c07d1fccf6f5ed71cfca9c125b0d649be97e90
Jessica Wade <jessica.wade@parameter1.com>
2021-11-02 09:50:16 -0500

    Add NRWA info to brands.js file

--- tenants/all/config/brands.js
+++ tenants/all/config/brands.js
@@ -1,3 +1,17 @@
 module.exports = {
-
+  nrwa: {
+    brandName: 'National Rural Water Association',
+    logoSrc: '/files/base/roguemonkeymedia/all/image/static/nrwa/Rural-Water-Wire_rev-logo.svg',
+    tagline: 'Newsletter',
+    subscribeLink: '',
+    bgColor: '#3856a6',
+    socialMedia: {
+      imagePath: '/files/base/roguemonkeymedia/all/image/static/nrwa/newsletter/',
+      links: [
+        { provider: 'facebook', href: 'https://www.facebook.com/NationalRuralWaterAssociation/', target: '_blank' },
+        { provider: 'twitter', href: 'https://twitter.com/NRWA', target: '_blank' },
+        { provider: 'linkedin', href: 'https://www.linkedin.com/company/national-rural-water-associaiton/', target: '_blank' },
+      ],
+    },
+  },
 };
