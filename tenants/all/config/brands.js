916cc09f684f662bba8042866fac0054fcb92082
Jessica Wade <jessica.wade@parameter1.com>
2021-11-19 10:40:10 -0600

    Add advertise and editorial links to footer

--- tenants/all/config/brands.js
+++ tenants/all/config/brands.js
@@ -4,8 +4,8 @@ module.exports = {
     headerSrc: '/files/base/roguemonkeymedia/all/image/static/nrwa/RuralWaterWire-1200x454.png',
     logoSrc: '/files/base/roguemonkeymedia/all/image/static/nrwa/Rural-Water-Wire_rev-logo.svg',
     subscribeLink: '',
-    advertiseLink: '',
-    editorialLink: '',
+    advertiseLink: 'https://content-nrwa-org.www.base.parameter1.com/page/advertise',
+    editorialLink: 'https://content-nrwa-org.www.base.parameter1.com/page/contact-us',
     bgColor: '#3856a6',
     socialMedia: {
       imagePath: '/files/base/roguemonkeymedia/all/image/static/nrwa/newsletter/',
