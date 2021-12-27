a9c64a39c1e471aff9c084103d1726c3091c6342
Jessica Wade <jessica.wade@parameter1.com>
2021-12-27 11:17:02 -0600

    Update footer links

--- tenants/all/config/brands.js
+++ tenants/all/config/brands.js
@@ -3,9 +3,8 @@ module.exports = {
     brandName: 'National Rural Water Association',
     headerSrc: '/files/base/roguemonkeymedia/all/image/static/nrwa/RuralWaterWire-1200x454.png',
     logoSrc: '/files/base/roguemonkeymedia/all/image/static/nrwa/Rural-Water-Wire_rev-logo.svg',
-    subscribeLink: '',
     advertiseLink: 'https://content-nrwa-org.www.base.parameter1.com/page/advertise',
-    editorialLink: 'https://content-nrwa-org.www.base.parameter1.com/page/contact-us',
+    contactUslLink: 'https://content-nrwa-org.www.base.parameter1.com/page/contact-us',
     bgColor: '#3856a6',
     socialMedia: {
       imagePath: '/files/base/roguemonkeymedia/all/image/static/nrwa/newsletter/',
