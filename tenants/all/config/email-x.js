e815a88f7c317b54f64b5f1d6d14bb64a7d5bb91
Jessica Wade <jessica.wade@parameter1.com>
2024-08-28 08:52:11 -0500

    Use mindful uri

--- tenants/all/config/email-x.js
+++ tenants/all/config/email-x.js
@@ -1,6 +1,6 @@
 const EmailXConfiguration = require('@parameter1/base-cms-marko-newsletters-email-x/config');
 
-const config = new EmailXConfiguration(process.env.EMAILX_SERVE_URI || 'https://roguemonkeymedia.serve.email-x.parameter1.com');
+const config = new EmailXConfiguration('https://delivery.mindfulcms.com/roguemonkeymedia/default/compat/email-banner');
 
 config
   .setAdUnits('rural-water-wire', [
